453234b8935518485b177529353f7d3026730c80
Lucas Almeida <lucasaclima03@gmail.com>
2021-11-30 08:14:17 -0300

    adicionando stream de arquivos

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
+
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
     constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
     handle(request: Request, response: Response): Response {
-        const file = request;
+        const { file } = request;
         this.importCategoryUseCase.execute(file);
         return response.send();
     }
