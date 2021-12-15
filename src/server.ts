e02a0132a505e92baf63c96c3d61713de709c7c0
Lucas Almeida <lucasaclima03@gmail.com>
2021-12-14 22:16:03 -0300

    adicionando erro personalizado

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
@@ -6,12 +7,28 @@ import swaggerFile from "./swagger.json";
 
 import "./database";
 import "./shared/container";
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server error - ${err.message}`,
+        });
+    }
+);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
