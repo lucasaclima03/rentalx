455e0d733d1e1356a540641297fb5dba4864d01c
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-21 17:11:39 -0300

    isolando o banco de dados

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,7 +5,7 @@ import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
 
-import "../../../database";
+import "@shared/infra/typeorm";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 
