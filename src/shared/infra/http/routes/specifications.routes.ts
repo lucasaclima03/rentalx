5b9a6d5a8333959e815fa6ea5740b91ca541627c
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-21 16:58:27 -0300

    refatorando a estrutura do projeto

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,9 +1,11 @@
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { request, response, Router } from "express";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
-import swaggerFile from "./swagger.json";
+import swaggerFile from "../../../swagger.json";
 
-import "./database";
+import "../../../database";
 import "@shared/container";
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 
