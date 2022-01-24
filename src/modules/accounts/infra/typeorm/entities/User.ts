6af57e50cbf379437a9892a14df00419f07512b0
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-24 11:38:10 -0300

    adição da migration CreateCars

--- src/modules/accounts/infra/typeorm/entities/User.ts
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 @Entity("users")
 class User {
